fcfa16ce83558f268576f498d43764cbfd6eae34
adityasinghskit <adityasinghskit@gmail.com>
2022-05-02 12:33:59 +0530

    second commit

--- scripts/news.js
+++ scripts/news.js
@@ -5,9 +5,55 @@ document.querySelector("#header").innerHTML = navbar();
 let news = document.querySelector("#detailed_news");
 let data=JSON.parse(localStorage.getItem("news"));
 data=data[0];
-showNews(data);
+showNews1(data);
+let input=document.querySelector("#search_input");
+input.addEventListener("keypress",function(event){
+    if(event.key=="Enter"){
+        searchResults(input.value);
+        localStorage.setItem("search",input.value);
+        window.location.href="search.html";
+    }
+})
+async function searchResults(val){
 
-function showNews(data) {
+    try{
+        let res= await fetch(`https://masai-mock-api.herokuapp.com/news?q=${val}`);
+        let data=await res.json();
+        data=data.articles;
+        console.log(data);
+        showNews(data);
+    }
+    catch(err){
+        console.log("err:",err);
+    }
+}
+
+function showNews(data){
+    results.innerHTML=null;
+    data.forEach((el) => {
+        let {urlToImage,title,description}=el;
+        let card=document.createElement("div");
+        card.setAttribute("class","news");
+        card.onclick = function () {
+            seeNews(el);
+        }
+        let div1=document.createElement("div");
+        let img1=document.createElement("img");
+        img1.src=urlToImage;
+        div1.append(img1);
+        let div2=document.createElement("div");
+        let p1=document.createElement("p");
+        p1.innerText=title;
+        p1.style.fontWeight="600";
+        let p2=document.createElement("p");
+        p2.innerText=description;
+        div2.append(p1,p2);
+        card.append(div1,div2);
+        results.append(card);
+    })
+}
+
+function showNews1(data) {
     news.innerHTML = null;
         let { urlToImage, title, description,content } = data;
         
